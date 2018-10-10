1543bfb87bc63f91f404382322f0d85db03f0a62
Md. Mazharul Islam <mazharkafi@gmail.com>
2018-10-11 02:03:00 +0600

    Add solution for problem `A - Vasya and Socks` Link: https://codeforces.com/contest/460/problem/A Tags: brute force, implementation, math, *900 Ref: https://codeforces.com/contest/460/submission/44092144

---
+++ codeforces/460/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+     int a,b,x;
+
+                cin>> a>> b;
+
+                   x=a+((a-1)/(b-1));
+
+               cout<<x<<endl;
+}
