3020bb6afb25ff005ca3331fd9e24efeccafc2f4
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-02-04 02:04:00 +0600

    Add solution for problem `A - Compote` Link: https://codeforces.com/contest/746/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/746/submission/49439084

--- codeforces/746/A.cpp
+++ codeforces/746/A.cpp
@@ -1,29 +1,24 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-int a,b,c,x,y,z,d,e,f,g;
-cin>>a>>b>>c;
-x=a/1; //1
-y=b/2; //500
-z=c/4; //250
- if(x==y && y==z && z== x) {
-    g=x*1+x*2+x*4;
-    cout<<g<<endl;
-}
+int main(){
+    int a,b,c,r;
+    cin >> a;
+    cin >> b;
+    cin >> c;
 
-else if(y<=z && y<=x) {
-    e=y*1+y*2+y*4;
-    cout<<e<<endl;
-}
-else if(z<=x && z<=y) {
-    f=z*1+z*2+z*4;
-    cout<<f<<endl;
-}
+    int i=a;
+    while(1){
 
-else if(x<=y && x<=z) {
-    d=x*1+x*2+x*4;
-    cout<<d<<endl;
-}
+        if(b/(i*2)>=1 && c/(i*4)>=1){
+            r=i*7;
+            break;
+        }
+        i--;
+        if(i==0){
+            r=0;
+            break;
+        }
+    }
+    cout << r <<endl;
 }
