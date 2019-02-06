691800707a2e35703711e76fe49f5ea746376ade
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-02-06 14:43:00 +0600

    Add solution for problem `A - Translation` Link: https://codeforces.com/contest/41/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/41/submission/49522036

---
+++ codeforces/41/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    string a,b;
+    cin >> a;
+    cin >> b;
+    reverse(a.begin(),a.end());
+    if(b == a)cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
