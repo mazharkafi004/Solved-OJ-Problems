082956bb399f1abcb877eedb37cac6e1f5433747
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-02-22 15:02:00 +0600

    Add solution for problem `A - Bear and Big Brother` Link: https://codeforces.com/contest/791/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/791/submission/50310366

---
+++ codeforces/791/A.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+int a,b,i=0;
+cin>>a>>b;
+
+    for(i=1;;i++){
+            a*=3;
+            b*=2;
+
+            if(a>b) {
+
+
+                cout<<i<<endl;
+                 return 0;
+            }
+        }
+    }
