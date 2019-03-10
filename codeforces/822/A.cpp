ab9ba31687f556afaf82879789c5187d1aa056bd
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-03-10 21:09:00 +0600

    Add solution for problem `A - I'm bored with life` Link: https://codeforces.com/contest/822/problem/A Tags: implementation, math, number theory, *800 Ref: https://codeforces.com/contest/822/submission/51134107

--- codeforces/822/A.cpp
+++ codeforces/822/A.cpp
@@ -1,26 +1,20 @@
 #include<bits/stdc++.h>
 
- using namespace std;
+using namespace std;
 
- int main()
-
- {
-
-   int a,b,c=1,d=1,i,j;
-   cin>>a>>b;
-  if(a<b) {
-  for (i=1;i<=a;i++ )
-        {
-           c=c*i;
-        }
+int main() {
+int a,b,c=1,d=1,i;
+cin>>a>>b;
+if(a>b) {
+    for(i=1;i<=b;i++) {
+        c=c*i;
+    }
     cout<<c<<endl;
- }
- else
- {
-  for (j=1;j<=b;j++ )
-        {
-           d=d*j;
-        }
+}
+else {  for(i=1;i<=a;i++) {
+        d=d*i;
+    }
     cout<<d<<endl;
- }
- }
+}
+
+}
