9be98bef0d02cf6d59312ae5630e6d7c6a3d023f
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-03-11 13:03:00 +0600

    Add solution for problem `A - Comparing Strings` Link: https://codeforces.com/contest/186/problem/A Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/186/submission/51150060

--- codeforces/186/A.cpp
+++ codeforces/186/A.cpp
@@ -2,20 +2,13 @@
 using namespace std;
 string s,b;
 int ans;
-int main () { 
-//int ans;
-//string s,b;
-cin>>s>>b;
-for(int i=0; i<s.size(); i++){
-if (s[i]!=b[i]){
-ans++;}}
-
-sort(s.begin(),s.end());
-sort(b.begin(),b.end());
-
-if(s==b && ans==2 ) {
-cout<<"YES";}
-else {
-cout<<"NO";}
-return  0;
+int main (){
+	cin>>s>>b;
+	for(int i=0; i<s.size(); i++){
+	if (s[i]!=b[i]){ans++;}}
+	sort(s.begin(),s.end());
+	sort(b.begin(),b.end());
+	if(s==b && ans==2 ) cout<<"YES";
+	else cout<<"NO";
+	return  0;
 }
