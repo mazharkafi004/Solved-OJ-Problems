68600762f26debe7df4b1edf13ac187066aba539
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-03-30 01:59:00 +0600

    Add solution for problem `A - Arpa’s hard exam and Mehrdad’s naive cheat` Link: https://codeforces.com/contest/742/problem/A Tags: implementation, math, number theory, *1000 Ref: https://codeforces.com/contest/742/submission/51990857

---
+++ codeforces/742/A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+int a=1,b,c,d,n,i;
+cin>>n;
+b=n%4;
+if(n==0) {
+    cout<<"1"<<endl;
+}
+else if(b==1) {
+    cout<<"8"<<endl;
+}
+else if(b==2) {
+    cout<<"4"<<endl;
+}
+else if(b==3) {
+    cout<<"2"<<endl;
+}
+else if(b==0) {
+    cout<<"6"<<endl;
+}
+
+}
