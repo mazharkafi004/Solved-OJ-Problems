9f04e6bfc913fc111c379b1895d4738b750a6e80
Md. Mazharul Islam <mazharkafi@gmail.com>
2019-07-07 15:59:00 +0600

    Add solution for problem `B - Relatively Prime Pairs` Link: https://codeforces.com/contest/1051/problem/B Tags: greedy, math, number theory, *1000 Ref: https://codeforces.com/contest/1051/submission/56661420

---
+++ codeforces/1051/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	long long a,b;
+	cin>>a>>b;
+	cout<<"YES"<<endl;
+	while(a<b) {
+        cout<<a<<" "<<a+1<<endl;
+        a+=2;
+	}
+	}
+
