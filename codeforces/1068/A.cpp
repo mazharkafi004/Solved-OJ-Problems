6fec7278d7ee8c64ac6f1e2c2dc630b5efd00709
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-04-22 16:37:00 +0600

    Add solution for problem `A - Birthday` Link: https://codeforces.com/contest/1068/problem/A Tags: math, *1400 Ref: https://codeforces.com/contest/1068/submission/77634212

---
+++ codeforces/1068/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+ long long n,m,k,l,x,y,z;
+ cin>>n>>m>>k>>l;
+y=(k+l+m-1)/m;
+if(y*m>n) {
+    cout<<-1<<endl;
+}
+else {
+    cout<<y<<endl;
+}
+ }
+
+
+
