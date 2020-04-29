9803b0a536e7be3504c7bc043b94651756ebc113
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-04-29 21:30:00 +0600

    Add solution for problem `A - PolandBall and Hypothesis` Link: https://codeforces.com/contest/755/problem/A Tags: brute force, graphs, math, number theory, *800 Ref: https://codeforces.com/contest/755/submission/78492529

---
+++ codeforces/755/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+long long n,m;
+cin>>n;
+if(n==2) {
+    cout<<4<<endl;
+}
+else if(n==1){
+    cout<<5<<endl;
+}
+else {
+    cout<<n-2<<endl;
+}
+
+
+}
