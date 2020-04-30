b956b1a34c4faa763b54b87c91299ab52ae93b32
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-05-01 00:31:00 +0600

    Add solution for problem `B - Water Lily` Link: https://codeforces.com/contest/1199/problem/B Tags: geometry, math, *1000 Ref: https://codeforces.com/contest/1199/submission/78595712

---
+++ codeforces/1199/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ double h,l;
+cin>>h>>l;
+//h=h*1.000000;
+//l=l*1.000000;
+double a=((l*l)-(h*h)),b;
+b=a/(2*h);
+printf("%lf",b);
+//cout<<b<<endl;
+
+}
