21c16b1c7622366b26ca8f6d59f21b1eb7d9cc83
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-05-03 12:36:00 +0600

    Add solution for problem `A - Another One Bites The Dust` Link: https://codeforces.com/contest/1148/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1148/submission/78868454

---
+++ codeforces/1148/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+long long a,b,c;
+cin>>a>>b>>c;
+if(a==b) {
+    cout<<a+b+(2*c)<<endl;
+}
+else if(a>b) {
+    cout<<(2*b)+(2*c)+1<<endl;
+}
+else {
+    cout<<(2*a)+(2*c)+1<<endl;
+}
+
+}
