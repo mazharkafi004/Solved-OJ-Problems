5505d1ff23e5045a1a45cb0aa8ef1c8cf647726c
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-05-07 17:00:00 +0600

    Add solution for problem `A - MUH and Sticks` Link: https://codeforces.com/contest/471/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/471/submission/79260984

---
+++ codeforces/471/A.cpp
@@ -0,0 +1,52 @@
+
+
+
+          //FUCK STOPSTALK
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+long long t=6,p[6],a=0,b=0,c=0;
+for(long long i=0;i<t;i++) {
+    cin>>p[i];
+}
+sort(p,p+t);
+b=0;c=0;
+for(long long i=0;i<t;i++) {
+   for(long long j=0;j<t;j++) {
+    if(p[j]==p[i]){
+        b++;
+    }
+    if(b>c) {
+        c=b;
+        a=p[i];
+    }
+}
+b=0;
+}
+if(c<4) {
+    cout<<"Alien"<<endl;
+    return 0;
+}
+long long x=0,y=0,z=0;
+for(long long i=0;i<t;i++) {
+    if(p[i]!=a) {
+        x++;
+        if(x==1) {
+            y=p[i];
+        }
+        else {
+            z=p[i];
+        }
+    }
+}
+if(y==z) {
+    cout<<"Elephant"<<endl;
+}
+else {
+    cout<<"Bear"<<endl;
+}
+}
