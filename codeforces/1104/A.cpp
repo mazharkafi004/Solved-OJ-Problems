5332a4f983574a3edc5f81908efcebcb1743e27a
Md. Mazharul Islam <mazharkafi@gmail.com>
2020-06-01 23:34:00 +0600

    Add solution for problem `A - Splitting into digits` Link: https://codeforces.com/contest/1104/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/1104/submission/82243329

---
+++ codeforces/1104/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+long long n,m,a;
+cin>>n;
+
+
+
+cout<<n<<endl;
+for(long long i=0;i<n;i++) {
+    cout<<1<<" ";
+}
+cout<<endl;
+
+}
