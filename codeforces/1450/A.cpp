2560bcfdfcd2e9d2860a89749492f6105f57897b
Md. Mazharul Islam <mazharkafi@gmail.com>
2021-01-20 17:41:00 +0600

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/104899240

---
+++ codeforces/1450/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        long long n;
+        cin>>n;
+        string s;
+        cin>>s;
+      sort(s.begin(),s.end());
+      cout<<s<<endl;
+    }
+}
